75d40a3fd545190a64865029d87106e96a200185
MirShukhman <miriysha@gmail.com>
2023-08-18 19:59:04 +0300

    Sql Database Addition

---
+++ SQLDtatbase_BookLibraryManagment/BookLibraryManagment.sql
@@ -0,0 +1,33 @@
+use master
+
+create database BookLibraryManagment
+
+use BookLibraryManagment
+
+create table Books
+(ID int primary key identity(1,1)
+,title varchar(100)not null
+,author varchar(100) not null
+,pub_year int 
+,loan_type int not null
+)
+
+select * from Books
+
+create table Customers
+(ID int primary key identity(1,1)
+,name varchar(100) not null
+,city varchar(100)
+,age int 
+)
+
+select * from Customers
+
+create table Loans
+(custID int foreign key references Customers(ID) 
+,bookID int foreign key references Books(ID) 
+,loan_Date date not null
+,return_Date date
+)
+
+select * from Loans
